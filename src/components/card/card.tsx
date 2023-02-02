52920e34d2d942e4bdeee5fa98a55aaf6b528d20
Ieva <ieieieva.and@gmail.com>
2023-02-02 17:07:34 +0200

    initial commit

---
+++ src/components/card/card.tsx
@@ -0,0 +1,14 @@
+ import React from 'react';
+// import App from '../../App';
+// import './card.css'
+
+
+// export default function Card() {
+//   return (
+//     <div className='card__container'>
+//       <div className='card'>
+        
+//       </div>
+//     </div>
+//   )
+// }
---
+++ src/components/image/image.tsx
@@ -0,0 +1,14 @@
+ import React from 'react';
+// import App from '../../App';
+// import './image.css'
+
+// export default function Image() {
+//   return (
+//     <div className='image__container'>
+//       <img
+//         className='card__image' 
+//         src='https://images.immediate.co.uk/production/volatile/sites/30/2018/08/mango-fee0d79-e1648560084294.jpg?quality=90&resize=768,574'>
+//       </img>
+//     </div>
+//   )
+// }
